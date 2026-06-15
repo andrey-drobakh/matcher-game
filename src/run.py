3a443e4b686ba8be0b279df32d9c2c19244506a9
Andrey Drobakh <drobahandrey7@gmail.com>
2026-06-15 20:24:22 +0500

    Added multiple matches per one program execution:
    - solved #3;
    - renamed run_game => run_game_match;
    - added run_app function;

--- src/run.py
+++ src/run.py
@@ -8,16 +8,18 @@ import src.cli_game.common
 # from src.cli_game.backend import CLIGameBackend
 
 
-def run_game(
+def run_game_match(
         game_backend : core.AbstractGameBackend,
         game_interface : core.AbstractGameInterface,
-        game_data : core.AbstractGameData
+        game_data : core.AbstractGameData,
+        print_intro_text : bool = True,
 ) :
     be = game_backend
     i = game_interface
     data = game_data
 
-    i.print_intro_text( data )
+    if print_intro_text :
+        i.print_intro_text( data )
 
     if not i.read_and_handle_setup_data( data ) :
         return
@@ -36,19 +38,49 @@ def run_game(
     i.display_game_results( data )
 
 
+def run_app() :
+    print_intro_text = True
+    while True :
+        run_game_match(
+            CLIGame_SampleBackend(),
+            CLIGameInterface(),
+            src.cli_game.common.GameData(),
+            print_intro_text
+        )
+
+        quit_app = False
+
+        while True :
+            play_again = input( '\nPlay again? [y/n] : ' )
+            match play_again.strip().lower() :
+                case 'yes' | 'y' :
+                    print_intro_text = False
+
+                    break
+                case 'no' | 'n' :
+                    quit_app = True
+
+                    break
+                case _ :
+                    print( 'error : type \'yes\', \'no\' or just \'y\' or \'n\'' )
+
+        if quit_app :
+            break
+
+    print( '\nBye!' )
+
+
 if __name__ == '__main__' :
-    # run_game(
+    # run_game_match(
     #     sg.SampleBackend(),
     #     sg.SampleInterface(),
     #     sg.GameData()
     # )
 
-    run_game(
-        CLIGame_SampleBackend(),
-        CLIGameInterface(),
-        src.cli_game.common.GameData()
-    )
+    # run_game_match(
+    #     CLIGame_SampleBackend(),
+    #     CLIGameInterface(),
+    #     src.cli_game.common.GameData()
+    # )
 
-    # For now, this call uses "empty" objects,
-    # and it causes running the infinite loop!!!
-    # run_game( CLIGameBackend(), CLIGameInterface() )
+    run_app()
